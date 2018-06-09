584cea09547ac8ff7e4d7c81e63275cd3abba438
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-06-09 18:15:15 +0300

    Context

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new BootBot({
     appSecret: options.appSecret
 });
 
-
+var context = {};
 
 const wit = new Wit({
     accessToken: options.witToken,
@@ -23,32 +23,35 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function operationSelector(chat, data) {
+function operationSelector(conext, data) {
     // Get first & most probable entity
-    if("intent_operation" in data["entities"])
+    let operationValue;
+    if("intent_operation" in data["entities"]) {
         var intentOperation = data["entities"]["intent_operation"][0];
-    else
-        return "Intent operation not recognised";
+        operationValue = intentOperation["value"];
+    }
+    else {
+        if("intent-type" in context) {
+            operationValue = context["intent-type"];
+        } else {
+            return "Intent operation not recognised";
+        }
+    }
 
-    let operationValue = intentOperation["value"];
     let entities = data["entities"];
 
     // Return the response ( a string with the message for the computed operation )
-    return operations[operationValue](chat, entities);
+    return operations[operationValue](context, entities);
 }
 
 // Users
 bot.on('message', (payload, chat) => {
     // Receive message from payload.sender.id
 
-    var a = 3;
     wit.message(payload.message.text, JSON.stringify(payload.message.nlp))
         .then((data) => {
             // write an answer to the user
-            chat.setAttribute('hello', 'world');
-            chat.say("Blabla");
-            // chat.say( operationSelector(chat, data) );
-            chat.end();
+            chat.say( operationSelector(context, data) );
         })
         .catch(console.error);
 });
