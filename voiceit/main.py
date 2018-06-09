a9a9d89b290f3e538e3cd5d77bcd6f5a85879bfd
Dragos Tudor <dragostudoriliuta@gmail.com>
2018-06-10 00:42:33 +0300

    Working structure

--- voiceit/main.py
+++ voiceit/main.py
@@ -6,25 +6,33 @@ from libs.VoiceIt import *
 from pydub import AudioSegment
 
 class VoiceChat():
-    def __init__(self, credentials_path):
+    def __init__(self, credentials_path, use_mp4, audio_path):
         # Initialise credentials 
-        credentials = json.load(open(credentials_path, 'r'))
-        self.Voice = VoiceIt(credentials['user_key'])
-        self.usr = credentials['usr']
-        self.pwd = credentials['pwd']
+        credentials     = json.load(open(credentials_path, 'r'))
+        self.Voice      = VoiceIt(credentials['user_key'])
+        self.usr        = credentials['usr']
+        self.pwd        = credentials['pwd']
+        self.mp4        = use_mp4
+        self.audio_path = audio_path
 
     def get_url_templates(self):
         # Url templates from a predefined path. Wav format required.
         return ['recordings/training/%s' % f for f in os.listdir('recordings/training/') if 'rec' in f]
 
-    def auth_audio_process_path(self, url, file_format='mp4'):
+    def auth_audio_process_path(self, file_format='mp4'):
         # Downloads facebook audio from given url and the desired format which is defaulted to 'mp4'
         raw_audio_path = 'recordings/auth/confirmation.%s' % file_format
-        urllib.request.urlretrieve (url, raw_audio_path)
-        sound = AudioSegment.from_file(raw_audio_path, file_format)
+        urllib.request.urlretrieve (self.audio_path, raw_audio_path)
 
-        export_path = "recordings/auth/confirmation.wav"
-        sound.export(export_path, format="wav")
+        return raw_audio_path if self.mp4 else convert_to_format(raw_audio_path, 'recordings/auth')
+
+
+    def convert_to_format(path_from, path_to, file_format='wav'):
+        # Converts audio file at given 'path_from' to a 'file_format' file found at 'path_to'.
+        sound = AudioSegment.from_file(path_from, file_format)
+        export_path = path_to + '/' + 'confirmation.%s' % file_format
+        sound.export(path_to, format="wav")
+
         return export_path
 
     def check_user(self):
@@ -36,6 +44,7 @@ class VoiceChat():
         print('-----------------')
         print('Enrolling training recordings')
         print('-----------------')
+
         templates = self.get_url_templates()
         for url in templates:
             response = self.Voice.createEnrollment(self.usr, self.pwd, os.getcwd() + '/' + url, "en-US")
@@ -46,15 +55,21 @@ class VoiceChat():
         print('-----------------')
         print('Deleting all user enrollments')
         print('-----------------')
+
         response = self.Voice.getEnrollment(self.usr, self.pwd)
         [self.Voice.deleteEnrollment(self.usr, self.pwd, str(s)) for s in json.loads(response)['Result']]
 
-    def authenticate(self, auth_audio_url):
+    def authenticate(self):
         # Authentication validation.   
         print('-----------------')
         print('User authentication')
         print('-----------------')
-        auth_audio_path = self.auth_audio_process_path(auth_audio_url)
+
+        auth_audio_path = self.auth_audio_process_path()
+
+        print('Authenticating with audio file from %s' % auth_audio_path)
+        print('-----------------')
+
         return self.Voice.authentication(self.usr, self.pwd, auth_audio_path, "en-US")
         
      
@@ -62,16 +77,17 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Pass on arguments for authentication.')
     parser.add_argument('audio_path', type=str, help='Path to the user authentication audio')
-    parser.add_argument('-enroll', type=bool, default=False, help='True - enrolls sessions in recordings/train, False - Default, authenticates model.')
+    parser.add_argument('-enroll', type=bool, default=False, help='True - enrolls sessions in recordings/train, False - (Default), authenticates model.')
     parser.add_argument('-credentials', type=str, default='credentials.json', help='Path to credentials.json') 
+    parser.add_argument('-use_mp4', type=bool, default=True, help='True (Default) - MP4 audio file for authenticating the user. False - WAV') 
 
     args = parser.parse_args()
 
-    voice = VoiceChat(args.credentials)
+    voice = VoiceChat(args.credentials, args.use_mp4, args.audio_path)
 
     if args.enroll:
         voice.create_templates()
 
-    response = voice.authenticate(args.audio_path)
+    response = voice.authenticate()
 
     print(response)
--- voiceit/readme.md
+++ voiceit/readme.md
@@ -5,10 +5,10 @@ Main authentication script in main.py
 
 python main.py [audio_path] [-enroll]
 
-audio_path (str) - url to the user's recorded audio
--enroll   (bool) - True - rebuilds training samples, False (default) - classifies audio file from audio_path 
+audio_path (str) - url to the user's recorded audio__
+-enroll   (bool) - True - rebuilds training samples, False (default) - classifies audio file from audio_path__
 
 key phrase = 'a perfect day really makes me happy'
 
-/recordings/training - at least 3 recordings of the user's voice pronouncing the key phrase 
-/recordings/auth     - new recording gets dumped here and converted to .wav. It is the processed by main.py
+/recordings/training - at least 3 recordings of the user's voice pronouncing the key phrase__
+/recordings/auth     - new recording gets dumped here and converted to .wav. It is the processed by main.py__
