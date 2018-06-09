079a870712f1dafee16587ad1e3d2b3ad3c2ff40
Alex Benchea <alexbenchea99@yahoo.com>
2018-06-09 21:27:35 +0300

    credit option message (test)

--- OperationsFunction/creditOptions.js
+++ OperationsFunction/creditOptions.js
@@ -1,6 +1,20 @@
-function addNewAccount(context, chat, entities) {
-
-    return "I will create a new account for your right away, can you just provide a currency ?";
+function creditOption(context, chat, entities) {
+    chat.sendTemplate({
+        "template_type": "list",
+        "top_element_style": "COMPACT",
+        "elements": [{
+            "title": "Test1",
+            "subtitle": "ama are mult emetet as a d",
+            },{
+            "title": "Test2",
+            "subtitle": "sagsg gds agas gs gsa g",
+            },
+            {
+            "title": "Viorica din pascani",
+            "subtitle": "asvgdsa gf saf sdf saf ",
+            }
+        ]
+    })           
 }
 
-exports.addNewAccount = addNewAccount;
+exports.creditOption = creditOption;
