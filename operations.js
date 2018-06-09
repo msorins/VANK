e65857b84d42a47a67164fd2f93311b510f1d8f9
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-06-09 22:39:36 +0300

    added new operation

--- operations.js
+++ operations.js
@@ -1,9 +1,11 @@
 const addNewAccount = require('./OperationsFunction/addNewAccount').addNewAccount;
 const showClosest = require('./OperationsFunction/showClosest').showsClosest;
+const creditOptions = require('./OperationsFunction/creditOptions').creditOption;
 
 const operations = {
     "show_closest" : showClosest,
-    "add_new_account": addNewAccount
+    "add_new_account": addNewAccount,
+    "credit_options": creditOptions
 };
 
 exports.operations = operations;
