fa2ee0e41fecc4e21fa46c94032884742164b1de
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-06-09 15:01:28 +0300

    Integrate WIT.  IBM = BAD

--- options.js
+++ options.js
@@ -1,7 +1,9 @@
-const accessToken = 'EAAGgS18RNa0BAMN9PvucMdN6P0bWF4ZAKI3uNzD6s6UP0vVQqiSZAAI2d970FAfCY6uXZAzCgBw9vzVZBGrHBzVhB9ekve4Kn5Pfr6InQy645vmu2GJZBq07sHCyK52C9W5dZCe1J2c4Cm2zg20NHK6Q55ZAcQK8bYdocGqw4byePTGJeDZBkhkU';
-const verifyToken = 'bingbangprintrecopacitokenmy';
-const appSecret = 'b60b030f476b72254cfb41151e571372';
+const accessToken = 'EAAWXfZBJzBOgBALMOTKRQSH6BNZC1b8sZAV1YxRzpUE59haN9lzMQlBL9PKJliI6hAA99dgvi0B9PYeFJkFeLcts3dsqy1R640xrn8wBLcUL8CNc1p9ep9sy0Pis20KJ8ZCarYOahV7lT1GmPDv1hJxUIQ5VcZB04BdAbI49a2ZCHCJtqqWT35';
+const verifyToken = 'soarecelmaifainb@T';
+const appSecret = '96e58c25650a7181b91eaf660c342c17';
+const witToken = '5HJKC5XHVWBFG24SKRCUDS6OMGBBT45U';
 
 exports.accessToken = accessToken;
 exports.verifyToken = verifyToken;
 exports.appSecret = appSecret;
+exports.witToken = witToken;
